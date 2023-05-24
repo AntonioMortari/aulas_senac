e8755d729c34c6d72e5f59a160ee8b6d9c3928ac
ANTONIO MARCOS MORTARI PESSOA <antonio.mmpessoa@senacsp.edu.br>
2023-05-24 19:15:13 -0300

    aulas

---
+++ javascript_15_05_2023/ex03/js/script.js
@@ -0,0 +1,19 @@
+console.log("Primeiro")
+
+console.log("Último")
+
+console.log("Mais um console log")
+
+//Declarar uma variável
+
+var a = 1
+
+a = "teste"
+
+console.log(a)
+
+/*este é um
+ comentário de 
+ múltiplas linhas
+*/
+
