699f921f202a3ebe6e26526cf346c7e254c540f5
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-06-20 00:01:05 -0300

    attt

--- projetos/darklightform/assets/js/script.js
+++ projetos/darklightform/assets/js/script.js
@@ -1,8 +1,17 @@
+
+// darkmode
 let btn = document.querySelector('#mode_icon')
 let body = document.getElementById('b')
 
 const darkmode = () =>{
-    body.classList.toggle('darkmode')
+    body.classList.toggle('active')
 }
 
 btn.addEventListener('click' , darkmode)
+
+
+//password button
+
+let btn_view =document.querySelector('#eyeview')
+
+btn_view.addEventListener('click
