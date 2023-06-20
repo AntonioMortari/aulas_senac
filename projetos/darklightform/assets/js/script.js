f8f7333f19005b5ed95cd632a3f67aa50ed68af6
Antonio Mortari <113060294+AntonioMortari@users.noreply.github.com>
2023-06-20 09:10:04 -0300

    att

--- projetos/darklightform/assets/js/script.js
+++ projetos/darklightform/assets/js/script.js
@@ -1,10 +1,13 @@
 
-// darkmode
+// darkmode + trocar botao
 let btn = document.querySelector('#mode_icon')
 let body = document.getElementById('b')
 
+
 const darkmode = () =>{
     body.classList.toggle('active')
+    btn.classList.toggle('fa-moon')
+    btn.classList.toggle('fa-sun')
 }
 
 btn.addEventListener('click' , darkmode)
@@ -13,5 +16,19 @@ btn.addEventListener('click' , darkmode)
 //password button
 
 let btn_view =document.querySelector('#eyeview')
+let input_password = document.querySelector('#password')
 
-btn_view.addEventListener('click
+const slasheye = () =>{
+    //trocar botão
+    btn_view.classList.toggle('fa-eye')
+    btn_view.classList.toggle('fa-eye-slash')
+
+    //mostrar senha
+    if(input_password.type == 'text'){
+        input_password.type = 'password'
+    }else{
+        input_password.type = 'text'
+    }
+}
+
+btn_view.addEventListener('click' , slasheye)
